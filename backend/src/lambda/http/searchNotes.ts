e3a05aecfef666ea17e38e0778743565ef401736
Vladimir Kuznetsov <vladimir-kazan@users.noreply.github.com>
2020-04-21 18:03:20 +0200

    Implement search using ElasticSearch

---
+++ backend/src/lambda/http/searchNotes.ts
@@ -0,0 +1,57 @@
+import 'source-map-support/register';
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as ElasticSearch from 'elasticsearch';
+
+import { createLogger, getWithCors } from '../../utils';
+import { SearchParams } from 'elasticsearch';
+import { NoteModel } from '../../dataLayer/notes.service';
+
+const logger = createLogger('searchNotes');
+const withCors = getWithCors({
+  origin: '*',
+  credentials: true,
+});
+
+const { ES_ENDPOINT = '' } = process.env;
+const ES_INDEX_NAME = 'search-notes';
+const ES_DOC_TYPE = 'markdown-document';
+
+const es = new ElasticSearch.Client({
+  hosts: [ES_ENDPOINT],
+  connectionClass: require('http-aws-es'),
+});
+
+const searchNotesHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  logger.info('Caller event', event);
+  const { expression = '' } = event.pathParameters || {};
+  const { authorizer } = event.requestContext || {};
+  const { principalId = '' } = authorizer || {};
+  const queryExpression = `(title:${expression}* OR body:${expression}*) AND userId:"${principalId}"`;
+  const params: SearchParams = {
+    index: ES_INDEX_NAME,
+    type: ES_DOC_TYPE,
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              query_string: {
+                query: queryExpression
+              }
+            }
+          ]
+        }
+      }
+    }
+  };
+  const result = await es.search<NoteModel>(params);
+  const items = result.hits.hits.map(i => i._source);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+  return withCors(response);
+};
+
+export const handler = searchNotesHandler;
