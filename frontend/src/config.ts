577212625de2ff0a18308ca45085410f08f139ea
Vladimir Kuznetsov <vladimir-kazan@users.noreply.github.com>
2020-05-24 21:53:54 +0200

    Update config.js FE

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ export const authConfig = {
   domain: 'vku713.auth0.com',
   clientId: 'j9orHymagS29sn65A5j2xZs8JySS4upc',
   // callbackUrl: 'http://localhost:3000/callback',
-  callbackUrl: 'http://notes-markdown-hosting-dev-v1.s3-website-us-east-1/callback',
+  callbackUrl: 'http://notes-markdown-hosting-dev-v1.s3-website-us-east-1.amazonaws.com/callback',
 };
 
 export const backendConfig = {
